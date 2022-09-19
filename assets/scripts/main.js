b779aa643c88c5fe14429187b41d936c5d87f16d
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2022-09-19 18:24:42 +0100

    Updated the style and navbar logic

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,66 +1,87 @@
 const app = () => {
-	const body = document.body;
+	const hamburger = document.querySelector(".hamburger");
+	const header = document.querySelector("header");
+	const navMenu = document.querySelector(".nav-menu");
 	const navBar = document.querySelector("nav");
-	const hamburgerMenu = document.querySelector('.hamburger-menu');
-	const navLinks = document.querySelector('.nav-links');
-	const navLink = document.querySelectorAll('.nav-item');
+	const body = document.body;
 
-	const navSlide = () => {
+	hamburger.addEventListener("click", mobileMenu);
 
-		hamburgerMenu.addEventListener('click', () => {
-			navLinks.classList.toggle("nav-active");
-			hamburgerMenu.classList.toggle('change');
-			body.classList.toggle('scroll-disabled')
-			navLink.forEach((link) => {
-				link.addEventListener('click', () => {
-					navLinks.classList.remove('nav-active');
-					hamburgerMenu.classList.remove('change');
-					body.classList.remove('scroll-disabled');
-				})
-			})
-		})
+	function mobileMenu() {
+		hamburger.classList.toggle("active");
+		navMenu.classList.toggle("active");
+		body.classList.toggle("scroll-disabled");
 	}
 
+	const navLink = document.querySelectorAll(".nav-link");
 
+	navLink.forEach((n) =>
+		n.addEventListener("click", () => {
+			hamburger.classList.remove("active");
+			navMenu.classList.remove("active");
+			body.classList.remove("scroll-disabled");
+		})
+	);
 
 	const makeNavRevealOnScrollUp = () => {
-
 		let lastScroll = 0;
-		window.addEventListener('scroll', () => {
+		window.addEventListener("scroll", () => {
 			const currentScroll = window.pageYOffset;
-			if (navLinks.classList.contains('nav-active')) {
-				body.classList = ''
+			if (navMenu.classList.contains("active")) {
+				body.classList = "";
 				// body.style.overflowY = 'hidden'
 			} else if (currentScroll <= 0) {
-				body.classList.remove('scroll-up')
-			} else if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
-				body.classList.remove('scroll-up');
-				body.classList.add('scroll-down')
-			} else if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
-				body.classList.remove('scroll-down');
-				body.classList.add('scroll-up')
+				body.classList.remove("scroll-up");
+			} else if (
+				currentScroll > lastScroll &&
+				!body.classList.contains("scroll-down")
+			) {
+				body.classList.remove("scroll-up");
+				body.classList.add("scroll-down");
+			} else if (
+				currentScroll < lastScroll &&
+				body.classList.contains("scroll-down")
+			) {
+				body.classList.remove("scroll-down");
+				body.classList.add("scroll-up");
 			}
 
-			lastScroll = currentScroll
-		})
-	}
+			lastScroll = currentScroll;
+		});
+	};
 
 	const fixBuggyNav = () => {
-		const x = window.matchMedia("min-width: 768px");
-		console.log('hello');
-	}
+		window.addEventListener("orientationchange", () => {
+			if (
+				screen.orientation.type === "landscape-primary" &&
+				window.matchMedia("(min-width: 768)")
+			) {
+				console.log("hello");
+				navMenu.classList.remove("active");
+				body.classList.remove("scroll-disabled");
+				hamburger.classList.remove("active");
+			}
+		});
+	};
 
-	navSlide();
+	const changeNavStyleOnScroll = () => {
+		window.onscroll = () => {
+			const gap = 50;
+			const homeSection = document.querySelector("#home");
+			if (
+				document.body.scrollTop > homeSection.clientHeight - 10 ||
+				document.documentElement.scrollTop > gap
+			) {
+				header.classList.add("inverse");
+			} else {
+				header.classList.remove("inverse");
+			}
+		};
+	};
+
+	changeNavStyleOnScroll();
+	fixBuggyNav();
 	makeNavRevealOnScrollUp();
-	screen.orientation.onchange =  (e) => {
-		// console.log(e);
-		if (window.matchMedia('(min-width: 768px)').matches) {
-			console.log('hello')
+};
 
-		}
-	}
-
-}
-
-
-app()
+app();
