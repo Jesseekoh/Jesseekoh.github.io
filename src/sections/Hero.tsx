891256d346a9b9f2ec74fe7ab341688585c507d4
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-06-12 23:57:11 +0100

    refactor: update Hero component layout and styling

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,44 +1,62 @@
-import { useEffect, useState } from "react";
-import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from 'react';
+import { Linkedin, Github } from 'lucide-react';
 const Hero = () => {
-  const [emoji, setEmoji] = useState('')
+  const [emoji, setEmoji] = useState('');
 
   useEffect(() => {
     setInterval(() => {
-      const emojiList = [
-        ..."😮😀😁😐😑😬😎"
-      ]
-      const now = new Date()
-      setEmoji(emojiList[Math.floor(now.getTime() / 200) % 7])
-    }, 200)
-
-  }, [])
+      const emojiList = [...'😮😀😁😐😑😬😎'];
+      const now = new Date();
+      setEmoji(emojiList[Math.floor(now.getTime() / 200) % 7]);
+    }, 200);
+  }, []);
 
   return (
-    <section className="hero py-40 min-h-svh grid place-content-center">
-      <div className="hero-text px-4">
-        <h1 className="text-2xl font-[Montserrat] font-extrabold pb-2">
-          <span className="font-mono inline-block pb-3">
-            Hi
-            <img src="https://em-content.zobj.net/source/microsoft-teams/363/waving-hand_1f44b.png" alt="waving hand emoji" className="inline pb-4" width={40} />
-
-            , I'm
-          </span>
-          <br />
-          <span className="text-3xl font-black tracking-tighter">{`Jesse Ekoh-Ordan ${emoji}`}
-          </span></h1>
-
-
-        <p className="pb-2">
+    <section id="hero">
+      <div className="container mx-auto h-svh max-h-[800px] grid place-content-center">
+        <div className="hero-text px-4">
+          <h1 className="text-2xl font-extrabold pb-2">
+            <span className="font-mono inline-block pb-3">
+              Hi, there
+              <img
+                src="https://em-content.zobj.net/source/microsoft-teams/363/waving-hand_1f44b.png"
+                alt="waving hand emoji"
+                className="inline pb-4"
+                width={40}
+              />
+            </span>
+            <br />
+            <span className="ml-8 text-7xl font-black tracking-tighter bg-cyan-600/90">
+              {`Jesse Ekoh-Ordan ${emoji}`}
+            </span>
+          </h1>
+          <p className="text-7xl text-center">Software Engineer</p>
+          {/* <p className="pb-2 text-lg leading-[1.3] tracking-wide">
           <span className="text-2xl">👨🏾‍💻</span>
-          FullStack Software Engineer
-        </p>
-        <a href="#" className="btn cta bg-[#d5693f] px-4 rounded-md py-2 inline-flex gap-2">Reach out
-          <PaperAirplaneIcon width={25} />
-        </a>
-      </div>
-    </section >
-  )
-}
+          FullStack Software Engineer Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Deleniti facere inventore, hic, dolorum ipsa a
+          asperiores ut minus fugit, esse repellendus nemo. Et voluptatibus ab
+          nostrum consequuntur cum voluptate tenetur dolor nisi, optio debitis,
+          nam non nesciunt iste neque? Quidem expedita dolore culpa aperiam quia
+          quis? Ipsam laboriosam obcaecati facere!
+        </p> */}
 
-export default Hero
+          <ul className="flex gap-4 justify-center items-center">
+            <li>
+              <a href="https://github.com/jesseekoh" target="_blank">
+                <Github className="size-8" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/jesseekoh" target="_blank">
+                <Linkedin className="size-8" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
